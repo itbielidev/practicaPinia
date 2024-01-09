9add1f86a4bf5bc3afbbe606db6570ba6aa774c0
carolina <carolina.blanco.7e6@itb.cat>
2024-01-09 19:51:16 +0100

    wip practica 2

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,16 +1,18 @@
 import { defineStore } from "pinia";
 
-export const useCartStore=defineStore("CartStore",{
-    state:()=>{
-        return{
-            items:[],
-        }
-    }
-})
+export const useCartStore = defineStore("CartStore", {
+  state: () => {
+    return {
+      items: [],
+    };
+  },
+});
 
-const addToCart = (count, product)=>{
-    count=parseInt(count)
-    for(let index=0;index<count;index++){
-        useCartStore.items.push(product);
+const addToCart = (count, product) => {
+  count = parseInt(count);
+  CartStore.$patch((state) => {
+    for (let index = 0; index < count; index++) {
+      useCartStore.items.push(product);
     }
-}
+  });
+};
